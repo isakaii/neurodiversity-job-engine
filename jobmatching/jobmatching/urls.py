f44f6f30231eb6557bbc3b1be73b46a624ab577f
Huayu <ihe210699@gmail.com>
2021-06-26 12:44:59 -0700

    Main page and bootstrap

--- jobmatching/jobmatching/urls.py
+++ jobmatching/jobmatching/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('resumes/', include('resumes.urls')),
+    path('', include('resumes.urls')),
     path('admin/', admin.site.urls),
 ]
