6743c91130085562429c221d83f53306c17747e9
Huayu <ihe210699@gmail.com>
2021-06-26 14:50:40 -0700

    pdf upload in progress

--- jobmatching/resumes/views.py
+++ jobmatching/resumes/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import PdfFileForm
 
 
 def index(request):
     return render(request, 'resumes/index.html')
 
+@login_required
 def upload(request):
+    if request.method == 'POST':
+        print(request)
+        print(request.FILES['pdfFile'])
+        form = PdfFileForm(request.POST, request.FILES)
+        print(form)
+        if form.is_valid():
+            print(form)
+            handle_pdf_upload(request.FILES['pdfFile'])
+    else:
+        form = PdfFileForm()
     return render(request, 'resumes/upload.html')
+
+def handle_pdf_upload(file):
+    for chunk in f.chunks():
+        print(chunk)
